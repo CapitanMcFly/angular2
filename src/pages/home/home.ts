1954840dfd3100bd1bc1256d6c68263a38715112
Rafael Quintana de Francisco <raquinta@pa.uc3m.es>
2017-03-03 11:04:16 +0100

    {}[]() binding, # variables, * directives

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,53 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+class Talk {
+  constructor(public name, public speaker){
+  }
+}
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  talk: Talk = new Talk ('Angular', 'Daniel');
 
+  talks: Array<Talk> = [
+    this.talk,
+    { name: 'Ionic', speaker: 'Javier' },
+    { name: 'AngularJS', speaker: 'Marcel' },
+    { name: 'Java', speaker: 'Ivar' },
+  ]
+
+  estilo = {
+    color: "orange"
+  };
+
+  grande = {
+    'font-size': 'xx-large'
+  };
+
+  constructor(public navCtrl: NavController) {
+    setTimeout(() => this.talk.speaker = 'Rafael', 3000);
+  }
+
+  color(){
+    if(this.talk.speaker === 'Daniel')
+      return 'red';
+    else
+      return 'green';
+  }
+
+  onClickMe($event) {
+    console.log('click!');
+    console.log($event);
+  }
+
+  onKey(name, $event){
+    console.log(name);
+    console.log($event);
   }
 
 }
